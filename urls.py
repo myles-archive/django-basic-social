d804bcc0d2d3505f4d35d931444031370eb29ce5
Myles Braithwaite <me@mylesbraithwaite.com>
2008-03-14 18:59:51 -0400

    Inital Django application.

---
+++ urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^basic/', include('basic.foo.urls')),
+
+    # Uncomment this for admin:
+#     (r'^admin/', include('django.contrib.admin.urls')),
+)
